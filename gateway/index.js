951e0a7e1f903609fb6dd730889612b8adbbe24f
Lenny Burdette <lenny@apollographql.com>
2022-02-24 15:31:34 -0800

    feature flagged fed2

--- gateway/index.js
+++ gateway/index.js
@@ -1,32 +1,36 @@
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
-import { ApolloGateway as ApolloGateway2 } from "@apollo/gateway";
-import { ApolloGateway as ApolloGateway1 } from "@apollo/gateway-1";
+import { ApolloGateway } from "@apollo/gateway";
 import Redis from "ioredis";
 
 const app = express();
 
 const redis = new Redis("redis:6379");
 
-const gateway1 = new ApolloGateway1();
-const server1 = new ApolloServer({ gateway: gateway1 });
+const gateway = new ApolloGateway();
+const server = new ApolloServer({ gateway });
 
-const gateway2 = new ApolloGateway2();
-const server2 = new ApolloServer({ gateway: gateway2 });
+const gatewayFed2 = new ApolloGateway();
+const serverFed2 = new ApolloServer({
+  gateway: gatewayFed2,
+  apollo: {
+    graphRef: "lenny-scratch-1@current-fed2",
+  },
+});
 
-await Promise.all([server1.start(), server2.start()]);
+await Promise.all([server.start(), serverFed2.start()]);
 
-const handler1 = server1.getMiddleware({ path: "/" });
-const handler2 = server2.getMiddleware({ path: "/" });
+const handler1 = server.getMiddleware({ path: "/" });
+const handler2 = serverFed2.getMiddleware({ path: "/" });
 
 app.all("/", async (req, res, next) => {
-  const rollout = parseFloat(await redis.get("gateway2rollout"));
+  const rollout = parseFloat(await redis.get("fed2rollout"));
 
   if (rollout > Math.random()) {
-    console.log("gateway 2");
+    console.log("Fed 2");
     handler2(req, res, next);
   } else {
-    console.log("gateway 1");
+    console.log("Fed 1");
     handler1(req, res, next);
   }
 });
