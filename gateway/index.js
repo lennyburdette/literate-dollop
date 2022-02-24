01aa81a48355beb93b14ba6c264ae36b5cca8729
Lenny Burdette <lenny@apollographql.com>
2022-02-24 15:59:56 -0800

    wip

--- gateway/index.js
+++ gateway/index.js
@@ -3,12 +3,8 @@ import { ApolloServer } from "apollo-server-express";
 import { ApolloGateway } from "@apollo/gateway";
 
 const app = express();
-
 const gateway = new ApolloGateway();
 const server = new ApolloServer({ gateway });
-
 await server.start();
-
-server.applyMiddleware({ app });
-
+server.applyMiddleware({ app, path: "/" });
 app.listen(4000, () => console.log(`Gateway running`));
